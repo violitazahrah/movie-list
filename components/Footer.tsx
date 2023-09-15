8e52864e1c059126096241e47813f9999e667d24
violitazahrah <violitaaditya155@gmail.com>
2023-09-15 16:45:06 +0700

    menghapus footer pada folder pages dan menambahkan detail rate film

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,24 +1,10 @@
-// const Footer = () => {
-//   return (
-//     <div className="text-center flex items-center justify-center bg-indigo-700">
-//       <p className="text-xl text-gray-800">
-//         &copy; Copyright 2023 <span className="font-bold">MovieSide</span>
-//       </p>
-//     </div>
-//   );
-// };
-
-// export default Footer;
-
 const Footer = () => {
   return (
-    <footer style={{ position: "fixed", padding: "10px", textAlign: "center", width: "100%", bottom: "0" }} className="bg-indigo-700">
-      <div>
-        <p className="text-xl text-gray-800">
-          &copy; Copyright 2023 <span className="font-bold">MovieSide</span>
-        </p>
-      </div>
-    </footer>
+    <div className="text-center h-10 flex items-center justify-center bg-indigo-700">
+      <p className="text-xl text-gray-800">
+        &copy; Copyright 2023 <span className="font-bold">MovieSide</span>
+      </p>
+    </div>
   );
 };
 
--- components/PopularMovie.tsx
+++ components/PopularMovie.tsx
@@ -8,7 +8,7 @@ const PopularMovie = ({ movies }) => {
   return (
     <div className="bg-indigo-400 container max-w-7xl mx-auto pb-10 px-4">
       <h1 className="text-white text-2xl mt-8 mb-5">Popular Movies</h1>
-      <div className="grid gap-4 sm: grid-cols-2 md: grid-cols-3 lg: grid-cols-5">
+      <div className="mt-15 md:p-2 grid md:grid-cols-2 gap-2 h-15  md:aspect-[4/3] lg:grid-cols-4 lg:p-4 lg:aspect-[3/4] sm: grid-cols-1 sm:p-1 sm: aspect-[2/3]">
         {movies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />
         ))}
