b586268ae41dff2d7c0c0b87603bd1ca5a7d2956
Nini Suryani Bakri <niisuryanibakri.3@gmail.com>
2023-09-22 23:10:00 +0800

    Memperbaiki card di halaman utama

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,17 +2,22 @@ import Image from "next/image";
 import Link from "next/link";
 
 const MovieCard = ({ movie }) => {
-  return (
-    <Link href={`/movie/${movie.id}`}>
-      <div className="bg-white shadow-md rounded-md cursor-pointer">
-        <Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} width={700} height={800} className="rounded-t-md" />
-        <div className="px-6 py-2">
-          <div className="font-bold text-xl mb-1">{movie.title}</div>
-          {/* <p className="text-indigo-700 text-base mb-1">{movie.release_date}</p> */}
-          <p className="font-bold text-m mb-1">Rate: {movie.vote_average}</p>
-        </div>
-      </div>
-    </Link>
-  );
+	return (
+		<Link href={`/movie/${movie.id}`}>
+			<div className="bg-white shadow-md rounded-md cursor-pointer card-container">
+				<Image
+					src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+					width={700}
+					height={800}
+					className="rounded-t-md"
+				/>
+				<div className="px-6 py-2">
+					<div className="font-bold text-xl mb-1">{movie.title}</div>
+					{/* <p className="text-indigo-700 text-base mb-1">{movie.release_date}</p> */}
+					<p className="font-bold text-m mb-1">Rate: {movie.vote_average}</p>
+				</div>
+			</div>
+		</Link>
+	);
 };
 export default MovieCard;
